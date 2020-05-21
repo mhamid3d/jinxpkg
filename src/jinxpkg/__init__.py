d20951dbb07d43085cd47ed1967bbe28405490f3
mhamid <mhamid3d@gmail.com>
2020-05-21 16:43:57 -0400

    latest integration changes for conda environment

--- src/jinxpkg/__init__.py
+++ src/jinxpkg/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import
 
-from .base import update_version, make_package_init, make_version_file
+from .base import update_version, make_version_init, make_version_file
